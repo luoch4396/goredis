b6b76d76b2685969a316638b4af0541178bafa8d
kar98k <49570782@qq.com>
2023-03-19 22:32:12 +0800

    手写redis 3-19

--- data/interface/dict.go
+++ data/interface/dict.go
@@ -3,6 +3,7 @@ package _interface
 type DictConsumer func(key string, val interface{}) bool
 
 // Dict redis-字典数据结构定义
+// TODO :可以考虑范型（虽然不知道go的范型有啥太大的作用）
 type Dict interface {
 	Get(key string) (valve interface{}, exists bool)
 	Size() int
