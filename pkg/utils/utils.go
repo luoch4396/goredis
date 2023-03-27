b205ad6035defb77d2e21ccc408b417a93b2de80
ak47 <lc11223344>
2023-03-27 12:13:44 +0800

    范型初试

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,14 +5,14 @@ import (
 )
 
 // NewStringBuilder 字符串拼接
-func NewStringBuilder[T []byte | string](strs ...T) string {
-	if strs == nil || len(strs) == 0 {
+func NewStringBuilder[T []byte | string](t ...T) string {
+	if t == nil || len(t) == 0 {
 		return ""
 	}
 	var stringBuilder strings.Builder
 	//stringBuilder.Grow(n * len(str))
-	for i := 0; i < len(strs); i++ {
-		stringBuilder.WriteString(string(strs[i]))
+	for i := 0; i < len(t); i++ {
+		stringBuilder.WriteString(string(t[i]))
 	}
 	return stringBuilder.String()
 }
