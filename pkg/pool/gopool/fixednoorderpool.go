6fd0201024cd742c37e02545a3ad3c7e3a96f9fc
ak47 <lc11223344>
2023-04-04 17:08:54 +0800

    增加性能测试代码,使用协程池

--- pkg/pool/gopool/fixednoorderpool.go
+++ pkg/pool/gopool/fixednoorderpool.go
@@ -1,5 +1,11 @@
 package gopool
 
+import (
+	"goredis/pkg/log"
+	"runtime"
+	"unsafe"
+)
+
 // FixedNoOrderPool .
 type FixedNoOrderPool struct {
 	chTask       chan func()
@@ -10,6 +16,18 @@ func (np *FixedNoOrderPool) taskLoop() {
 	for f := range np.chTask {
 		call(f)
 	}
+}
+
+func call(f func()) {
+	defer func() {
+		if err := recover(); err != nil {
+			const size = 64 << 10
+			buf := make([]byte, size)
+			buf = buf[:runtime.Stack(buf, false)]
+			log.Errorf("taskpool call failed: %v\n%v\n", err, *(*string)(unsafe.Pointer(&buf)))
+		}
+	}()
+	f()
 }
 
 // Go .
